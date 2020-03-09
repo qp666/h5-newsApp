d6b3b35c6712bc20892367359f0fd754b54a2b60
qqpp <4399.com>
2020-03-09 11:40:22 +0800

    登录按钮点击的验证/阿里icon图标的使用

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './style/base.css'
+import './style/icon.css'
+
 //导入vant
 import Vant from 'vant';
 import 'vant/lib/index.css';
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,32 @@
 <template>
-  <div><h1>我是登录</h1></div>
+  <div class="login">
+    <!--导航栏 -->
+    <van-nav-bar title="登录" />
+
+    <!--输入框 -->
+    <van-field
+      v-model="form.phone"
+      placeholder="请输入手机号"
+      :error-message="valid.phone"
+    >
+      <template slot="left-icon">
+        <span class="iconfont icon-phone02"></span>
+      </template>
+    </van-field>
+    <van-field
+      v-model="form.code"
+      placeholder="请输入验证码"
+      :error-message="valid.code"
+    >
+      <template slot="left-icon">
+        <span class="iconfont icon-lock"></span>
+      </template>
+      <van-button slot="button" round size="small">发送验证码</van-button>
+    </van-field>
+
+    <!-- 登录按钮 -->
+    <van-button type="info" size="large" @click="doLogin">登录</van-button>
+  </div>
 </template>
 
 <script>
@@ -9,10 +36,49 @@ export default {
   props: {},
   //数据
   data() {
-    return {};
+    return {
+      form: {
+        phone: "", //手机
+        code: "" //验证码
+      },
+      valid: {
+        phone: "", //手机
+        code: "" //验证码
+      }
+    };
   },
+
   //方法
-  methods: {},
+  methods: {
+    //登录
+    doLogin() {
+      if (this.yanz()) {
+        console.log("全部通过");
+      } else {
+        console.log("验证失败");
+      }
+    },
+    //验证方法
+    yanz() {
+      let flag = true;
+      //判断手机号格式是否正确
+      if (/0?(13|14|15|18|17)[0-9]{9}/.test(this.form.phone)) {
+        //如果正确就清空错误提示
+        this.valid.phone = "";
+      } else {
+        flag = false;
+        this.valid.phone = "请输入正确手机号";
+      }
+      if (this.form.code.length == 6) {
+        //
+        this.valid.code = "";
+      } else {
+        flag = false;
+        this.valid.code = "请输入正确验证码";
+      }
+      return flag;
+    }
+  },
   //计算属性
   computed: {},
   //过滤器
@@ -28,4 +94,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped lang="less">
+.login {
+  background-color: #f5f7f9;
+
+  .van-nav-bar {
+    background-color: #3e9df8;
+    margin-bottom: 20px;
+    .van-nav-bar__title {
+      color: #fff;
+    }
+  }
+
+  .van-button--round {
+    background-color: #ededed;
+  }
+  .van-button--large {
+    width: 90%;
+    margin: 30px 19px;
+  }
+}
+</style>
